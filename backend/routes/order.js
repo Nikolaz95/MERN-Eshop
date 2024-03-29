f9ec2b095d2b7161bee40bd0f0f8c0771d4c04ed
Nikola Zovko <nikolajoe95@gmail.com>
2024-03-30 00:05:56 +0100

    3updejt1

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { authorizeRoles, isAuthenticatedUser } from "../middlewares/auth.js";
-import { allOrders, deleteOrder, getOrderDetails, MyOrders, newOrder, updateOrder } from "../controllers/orderControllers.js";
+import { allOrders, deleteOrder, getOrderDetails, getSales, MyOrders, newOrder, updateOrder } from "../controllers/orderControllers.js";
 const router = express.Router();
 
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder)
 router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails)
 router.route("/me/orders").get(isAuthenticatedUser, MyOrders)
+
+
+router
+.route("/admin/get_sales")
+.get(isAuthenticatedUser, authorizeRoles("admin"), getSales)
+
+
 
 router
 .route("/admin/orders")
