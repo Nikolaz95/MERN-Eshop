05fe9d87f608bda233e118fbb2533f4bf8fc39f0
Nikola Zovko <nikolajoe95@gmail.com>
2024-07-15 22:16:32 +0200

    dsgvgds123

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -7,31 +7,38 @@ export default (err, req, res, next) => {
     };
 
     //Handle invalid Mongoose ID error
- 
-    if(err.name === "CastError") {
+
+    if (err.name === "CastError") {
         const message = `Resource not found. Invalid: ${err?.path}`
         error = new ErrorHandler(message, 404)
     }
 
 
     //Handle Validation error
- 
-    if(err.name === "ValidationError") {
+
+    if (err.name === "ValidationError") {
         const message = Object.values(err.errors).map((value) => value.message);
         error = new ErrorHandler(message, 400)
     }
 
+    //Handle  Mongoose duplicate key error
 
-    if(process.env.NODE_ENV === "DEVELOPMENT") {
+    if (err.name === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+        error = new ErrorHandler(message, 404)
+    }
+
+
+    if (process.env.NODE_ENV === "DEVELOPMENT") {
         res.status(error.statusCode).json({
             message: error.message,
             error: err,
             stack: err?.stack,
         });
     }
-    if(process.env.NODE_ENV === "PRODUCTION") {
+    if (process.env.NODE_ENV === "PRODUCTION") {
         res.status(error.statusCode).json({
             message: error.message,
         });
-    } 
+    }
 };
